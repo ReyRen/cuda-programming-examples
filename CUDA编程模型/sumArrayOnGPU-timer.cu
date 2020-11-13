4bdbd08c2fc9d293af868b6ee24bf416692f7368
ReyRen <reyren179@gmail.com>
2020-11-13 18:03:43 +0800

    索引

---
+++ CUDA编程模型/sumArrayOnGPU-timer.cu
@@ -0,0 +1,82 @@
+#include <cuda_runtime.h>
+#include <stdio.h>
+#include <sys/time.h>
+
+int main(int argc, char **argv) {
+    printf("%s Starting...\n", argv[0]);
+
+    // set up device
+    int dev = 0;
+    cudaDeviceProp deviceProp;
+    CHECK(cudaGetDeviceProperties(&deviceProp, dev));
+    printf("Using Device %d: %s\n", dev, deviceProp.name);
+    CHECK(cudaSetDevice(dev));
+
+    // set up data size of vectors
+    int nElem = 1<<24;
+    printf("Vector size %d\n", nElem);
+
+    // malloc host memory
+    size_t nBytes = nElem * sizeof(float);
+
+    float *h_A, *h_B, *hostRef, *gpuRef;
+    h_A = (float *)malloc(nBytes);
+    h_B = (float *)malloc(nBytes);
+    hostRef = (float *)malloc(nBytes);
+    gpuRef = (float *)malloc(nBytes);
+
+    // initialize data at host side
+    iStart = cpuSecond();
+    initialData(h_A, nElem);
+    initialData(h_B, nElem);
+    iElaps = cpuSecond() - iStart;
+
+    memset(hostRef, 0, nBytes);
+    memset(gpuRef, 0, nBytes);
+
+    // add vector at host side for result checks
+    iStart = cpuSecond();
+    sumArraysOnHost(h_A, h_B, hostRef, nElem);
+    iElaps = cpuSecond() - iStart;
+
+    // malloc device global memory
+    float *d_A, *d_B, *d_C;
+    cudaMalloc((float **)&d_A, nBytes);
+    cudaMalloc((float **)&d_B, nBytes);
+    cudaMalloc((float **)&d_C, nBytes);
+
+    // transfer data from host to device
+    cudaMemcpy(d_A, h_A, nBytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, nBytes, cudaMemcpyHostToDevice);
+
+    // invoke kernel at host side
+    int iLen = 1024;
+    dim3 block(iLen);
+    dim3 grid((nElem + block.x - 1)/block.x);
+
+    iStart = cpuSecond();
+    sumArraysOnGPU<<<grid,block>>>(d_A, d_B, d_C, nElem);
+    cudaDeviceSynchronize();
+    iElaps = cpuSecond() - iStart;
+    printf("sumArraysOnGPU <<<%d, %d>>> Time elapsed %f" \
+      "sec\n", grid.x, block.x, iElaps);
+
+    // copy kernel result back to host side
+    cudaMemcpy(gpuRef, d_C, nBytes, cudaMemcpyDeviceToHost);
+
+    // check device results
+    checkResult(hostRef, gpuRef, nElem);
+
+    // free device global memory
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+
+    // free host memory
+    free(h_A);
+    free(h_B);
+    free(hostRef);
+    free(gpuRef);
+
+    return 0;
+}
